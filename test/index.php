937156ab6a14542919e1cc46a82fb1602c1e0758
nubyyy <84028775+nubyyy@users.noreply.github.com>
2022-03-17 13:06:14 -0400

    Update index.php

--- test/index.php
+++ test/index.php
@@ -2,11 +2,7 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 <script src="script.js"></script>
 <h1>test place for hmsanswers.com</h1>
-<?php
-$myfile = fopen("post.txt", "r");
-$txt = fread($myfile,filesize("post.txt"));
-fclose($myfile)
-?>
+
 <form action="formhandle.php" method="post">
   <label for="fchat">chat: </label>
   <input type="text" id="fchat"><br>
